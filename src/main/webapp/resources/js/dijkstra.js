1b7e05bf91304e4c5137a6421dea63639be7a57a
Rafael de Oliveira <rafah.528@gmail.com>
2019-12-11 23:32:26 -0300

    frontend

--- src/main/webapp/resources/js/dijkstra.js
+++ src/main/webapp/resources/js/dijkstra.js
@@ -1,19 +1,145 @@
-var sys = arbor.ParticleSystem(1000, 400,1);
-sys.parameters({gravity:true});
-sys.renderer = Renderer("#viewport");
-var a0= sys.addNode('a0', {'color':'blue','shape':'dot','label':'0'});
-var a1= sys.addNode('a1', {'color':'blue','shape':'dot','label':'1'});
-var a2= sys.addNode('a2', {'color':'blue','shape':'dot','label':'2'});
-var a3= sys.addNode('a3', {'color':'blue','shape':'dot','label':'3'});
-var a4= sys.addNode('a4', {'color':'blue','shape':'dot','label':'4'});
-var a5= sys.addNode('a5', {'color':'blue','shape':'dot','label':'5'});
-sys.addEdge('a0', 'a1',{'color':'black', 'weight':10});
-sys.addEdge('a0', 'a2',{'color':'black', 'weight':20});
-sys.addEdge('a1', 'a2',{'color':'black', 'weight':11});
-sys.addEdge('a1', 'a3',{'color':'black', 'weight':13});
-sys.addEdge('a2', 'a0',{'color':'black', 'weight':20});
-sys.addEdge('a2', 'a3',{'color':'black', 'weight':12});
-sys.addEdge('a2', 'a5',{'color':'black', 'weight':8});
-sys.addEdge('a3', 'a0',{'color':'black', 'weight':14});
-sys.addEdge('a3', 'a4',{'color':'black', 'weight':10});
-sys.addEdge('a4', 'a5',{'color':'black', 'weight':5});
+const filePicker = document.getElementById('file-picker');
+const uploadButton = document.getElementById('upload-file-button');
+const inputFile = document.getElementById('input-file');
+const main = document.getElementById('main');
+const loading = document.getElementById('loading');
+
+main.style.display = 'none';
+loading.style.display = 'none';
+
+uploadButton.addEventListener('click', () => {
+    console.log('upload');
+    doUpload();
+});
+
+
+function doUpload() {
+    loading.style.display = 'block';
+    filePicker.style.display = 'none';
+    const BASE_URL = "http://localhost:8080/grafo";
+
+    fetch(`${BASE_URL}/rest/grafo/upload`, {
+        method: 'POST',
+        body: inputFile.files[0]
+    })
+    .then(response => response.json())
+    .then((data) => {
+        main.style.display = 'flex';
+        loading.style.display = 'none';
+        processRoutes(data)
+    });
+
+
+}
+
+function processRoutes(data) {
+    // node var a0 = sys.addNode('a0', {'color': 'blue', 'shape': 'dot', 'label': '0'});
+    // edge sys.addEdge('a0', 'a1', {'color': 'black', 'weight': 10});
+    console.log(data);
+    const rotas = data.rotas;
+    const entrada = data.entrada;
+
+    let elements = [];
+    for (let vertice of entrada.verticesMatrizGrafo) {
+        // graph.addNode(vertice.nome, {'color': 'blue', 'shape': 'dot', 'label': vertice.nome});
+        elements.push({
+            data: {id: vertice.nome}
+        });
+        for (let aresta of vertice.arestas) {
+            // graph.addEdge(vertice.nome, aresta.verticeDestino, {'color': 'black', 'weight': aresta.comprimento});
+            if (vertice.nome === aresta.verticeDestino)
+                continue;
+            elements.push({
+                data: {
+                    id: vertice.nome + '_' + aresta.verticeDestino,
+                    source: vertice.nome,
+                    target: aresta.verticeDestino
+                }
+            });
+        }
+    }
+
+    const routeList = document.getElementById('route-list');
+    const routeDesc = document.getElementById('route-desc');
+
+    let count = 0;
+    for (let rota of rotas) {
+        count++;
+        let item = document.createElement('li');
+        item.className = 'route-list-item';
+        item.innerHTML = `<a href="#">${count}&ordf; rota ('A' at&eacute; '${rota.rotaMenor.destino}')</a>`;
+        routeList.appendChild(item);
+        registerItemClick(rota, item);
+    }
+
+    function registerItemClick(rota, item) {
+        item.addEventListener('click', () => {
+            routeDesc.innerHTML = `
+                <div>Menor rota: ${rota.rotaMenor.pontos.join('->')}</div>
+                <div>Tempo: ${rota.rotaMenor.distancia}</div>
+                <div>Recompensa: R$ ${rota.rotaMenor.recompensa}</div>
+            `;
+            createGraph(rota.rotaMenor);
+        })
+    }
+
+    routeList.children[0].click();
+
+    function createGraph(rotaAtual) {
+        var cy = cytoscape({
+
+            container: document.getElementById('viewport'), // container to render in
+
+            elements,
+            style: [ // the stylesheet for the graph
+                {
+                    selector: 'node',
+                    style: {
+                        'background-color': function (el) {
+
+                            let id = el.data('id');
+                            if (rotaAtual.pontos.some(p => p === id)) {
+                                return 'red';
+                            }
+                            return '#000'
+                        },
+                        'label': 'data(id)'
+                    }
+                },
+
+                {
+                    selector: 'edge',
+                    style: {
+                        'width': function (el) {
+                            let source = el.data('target');
+                            let target = el.data('source');
+                            let idx = rotaAtual.pontos.indexOf(source);
+                            if (idx >= 0 && (rotaAtual.pontos[idx + 1] === target || rotaAtual.pontos[idx - 1] === target)) {
+                                return 5;
+                            }
+                            return 2;
+                        },
+                        'line-color': function (el) {
+                            let source = el.data('target');
+                            let target = el.data('source');
+                            let idx = rotaAtual.pontos.indexOf(source);
+                            if (idx >= 0 && (rotaAtual.pontos[idx + 1] === target || rotaAtual.pontos[idx - 1] === target)) {
+                                return 'red';
+                            }
+                            return 'blue';
+                        },
+                        'target-arrow-color': '#ccc',
+                        'target-arrow-shape': 'triangle'
+                    }
+                }
+            ],
+
+            layout: {
+                name: 'random'
+            }
+        });
+    }
+}
+
+
+
